eca4eb95c935004deecc9d5e6ed9d8fda6158a2c
DanielaPF15 <dpfa1507@gmail.com>
2020-11-17 12:31:57 -0500

    listar productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,13 @@ import { SingUpComponent } from './Components/sing-up/sing-up.component';
 import { LoginComponent } from './Components/login/login.component';
 import { CreateBookComponent } from './Components/create-book/create-book.component';
 import { ListBookComponent } from './Components/list-book/list-book.component';
+import { AuthGuard } from './Guards/auth.guard';
+import { CreateGenreComponent } from './Components/create-genre/create-genre.component';
+import { UpdateBookComponent } from './Components/update-book/update-book.component';
 
 const routesApp:Routes = [
  {
-   path:'home',component:HomeComponent
+   path:'',component:HomeComponent
   },
  {
    path:'sing-up',component:SingUpComponent
@@ -27,11 +30,16 @@ const routesApp:Routes = [
     path: 'login', component: LoginComponent
   },
   {
-    path: 'create-book',component: CreateBookComponent
+    path: 'create-book',canActivate:[AuthGuard],data:{ only:'Admin'},component: CreateBookComponent
   },
   {
-    path: 'list-book',component: ListBookComponent
-  }
+    path: 'list-book',canActivate:[AuthGuard],component: ListBookComponent
+  },
+  {
+    path: 'create-genre',canActivate:[AuthGuard],data:{ only:'Admin'},component: CreateGenreComponent
+  },
+  { path: 'update-book/:id', canActivate: [AuthGuard], data: {only: 'Admin'}, component: UpdateBookComponent }
+
 
 ]
 @NgModule({
@@ -43,7 +51,9 @@ const routesApp:Routes = [
     SingUpComponent,
     LoginComponent,
     CreateBookComponent,
-    ListBookComponent
+    ListBookComponent,
+    CreateGenreComponent,
+    UpdateBookComponent
   ],
   imports: [
     BrowserModule,
